540603a24a29f46460606fee70ddcd9c91e93a7a
blancoCordero <94255996+blancoCordero@users.noreply.github.com>
2022-10-11 21:50:34 -0400

    metodo desplegarMenu que muestra el menu en la consola con las diferentes opciones para el usuario

--- src/blanco/adrian/bl/CuentaCorriente.java
+++ src/blanco/adrian/bl/CuentaCorriente.java
@@ -1,35 +1,35 @@
-package blanco.adrian.bl;
-
-public class CuentaCorriente {
-
-    //numero de cuenta debe ser maximo de 7 digitos
-    private String numeroCuenta;
-
-    private double saldo;
-
-    public CuentaCorriente(String numeroCuenta, double saldo) {
-        this.numeroCuenta = numeroCuenta;
-        this.saldo = saldo;
-    }
-    public void transaccionDeposito(double monto){
-        saldo += monto;
-    }
-    public void transaccionRetiro(double monto){
-        saldo -= monto;
-    }
-    public String getNumeroCuenta() {
-        return numeroCuenta;
-    }
-
-    public void setNumeroCuenta(String numeroCuenta) {
-        this.numeroCuenta = numeroCuenta;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
-}
+//package blanco.adrian.bl;
+//
+//public class CuentaCorriente {
+//
+//    //numero de cuenta debe ser maximo de 7 digitos
+//    private String numeroCuenta;
+//
+//    private double saldo;
+//
+//    public CuentaCorriente(String numeroCuenta, double saldo) {
+//        this.numeroCuenta = numeroCuenta;
+//        this.saldo = saldo;
+//    }
+//    public void transaccionDeposito(double monto){
+//        saldo += monto;
+//    }
+//    public void transaccionRetiro(double monto){
+//        saldo -= monto;
+//    }
+//    public String getNumeroCuenta() {
+//        return numeroCuenta;
+//    }
+//
+//    public void setNumeroCuenta(String numeroCuenta) {
+//        this.numeroCuenta = numeroCuenta;
+//    }
+//
+//    public double getSaldo() {
+//        return saldo;
+//    }
+//
+//    public void setSaldo(double saldo) {
+//        this.saldo = saldo;
+//    }
+//}
