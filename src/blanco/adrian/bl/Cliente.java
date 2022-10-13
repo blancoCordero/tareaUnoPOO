0363dd3f940e62eab8361857b7473502cff51463
blancoCordero <94255996+blancoCordero@users.noreply.github.com>
2022-10-13 01:57:51 -0400

    Finalizacion de las primeras 2 opciones del menu e inicio de la tercera, el usuario puede ahora registrar clientes y sus respectivas cuentas corrientes, con validacion de que el primer deposito sea mayor a 50000 y que el numero de cuenta no sea diferente a 7 digitos

--- src/blanco/adrian/bl/Cliente.java
+++ src/blanco/adrian/bl/Cliente.java
@@ -1,32 +1,45 @@
-//package blanco.adrian.bl;
-//import java.util.ArrayList;
-//
-//public class Cliente {
-//
-//    private String nombre;
-//
-//    private String identificacion;
-//
-//    private String fechaNacimiento;
-//
-//    // Edad? se calcula desde la fecha de nacimiento o se pregunta?
-//
-//    private String direccionHabitacion;
-//
-//    private ArrayList<CuentaCorriente> cuentas;
-//
-//    public Cliente(String nombre, String identificacion, String fechaNacimiento, String direccionHabitacion, ArrayList<CuentaCorriente> cuentas) {
-//        this.nombre = nombre;
-//        this.identificacion = identificacion;
-//        this.fechaNacimiento = fechaNacimiento;
-//        this.direccionHabitacion = direccionHabitacion;
-//        this.cuentas = cuentas;
-//    }
-//    //agregar cuenta al array
+package blanco.adrian.bl;
+import java.util.ArrayList;
+import java.util.Objects;
+
+import blanco.adrian.bl.CL;
+
+public class Cliente {
+
+    private String nombre;
+
+    private String identificacion;
+
+    private String fechaNacimiento;
+
+    // Edad? se calcula desde la fecha de nacimiento o se pregunta?
+
+    private String direccionHabitacion;
+
+    private ArrayList<CuentaCorriente> cuentas;
+
+    public Cliente(String nombre, String identificacion, String fechaNacimiento, String direccionHabitacion, ArrayList<CuentaCorriente> cuentas) {
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+        this.fechaNacimiento = fechaNacimiento;
+        this.direccionHabitacion = direccionHabitacion;
+        this.cuentas = cuentas;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(identificacion, cliente.identificacion);
+    }
+
+
+    //agregar cuenta al array
 //    public void agregarCuenta(){
 //        this.cuentas.add(new CuentaCorriente());
 //    }
-//
+
 //    public void deposito(int indice, double monto){
 //        cuentas[indice].transaccionDeposito(monto);
 //
@@ -42,57 +55,57 @@
 //    public double consultarSaldo(){
 //
 //    }
-//
-//    @java.lang.Override
-//    public java.lang.String toString() {
-//        return "Cliente{" +
-//                "nombre='" + nombre + '\'' +
-//                ", identificacion='" + identificacion + '\'' +
-//                ", fechaNacimiento='" + fechaNacimiento + '\'' +
-//                ", direccionHabitacion='" + direccionHabitacion + '\'' +
-//                ", cuentas=" + cuentas +
-//                '}';
-//    }
-//
-//    public String getNombre() {
-//        return nombre;
-//    }
-//
-//    public void setNombre(String nombre) {
-//        this.nombre = nombre;
-//    }
-//
-//    public String getIdentificacion() {
-//        return identificacion;
-//    }
-//
-//    public void setIdentificacion(String identificacion) {
-//        this.identificacion = identificacion;
-//    }
-//
-//    public String getFechaNacimiento() {
-//        return fechaNacimiento;
-//    }
-//
-//    public void setFechaNacimiento(String fechaNacimiento) {
-//        this.fechaNacimiento = fechaNacimiento;
-//    }
-//
-//    public String getDireccionHabitacion() {
-//        return direccionHabitacion;
-//    }
-//
-//    public void setDireccionHabitacion(String direccionHabitacion) {
-//        this.direccionHabitacion = direccionHabitacion;
-//    }
-//
-//    public ArrayList<CuentaCorriente> getCuentas() {
-//        return cuentas;
-//    }
-//
-//    public void setCuentas(ArrayList<CuentaCorriente> cuentas) {
-//        this.cuentas = cuentas;
-//    }
-//
-//
-//}
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Cliente{" +
+                "nombre='" + nombre + '\'' +
+                ", identificacion='" + identificacion + '\'' +
+                ", fechaNacimiento='" + fechaNacimiento + '\'' +
+                ", direccionHabitacion='" + direccionHabitacion + '\'' +
+                ", cuentas=" + cuentas +
+                '}';
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getDireccionHabitacion() {
+        return direccionHabitacion;
+    }
+
+    public void setDireccionHabitacion(String direccionHabitacion) {
+        this.direccionHabitacion = direccionHabitacion;
+    }
+
+    public ArrayList<CuentaCorriente> getCuentas() {
+        return cuentas;
+    }
+
+    public void setCuentas(ArrayList<CuentaCorriente> cuentas) {
+        this.cuentas = cuentas;
+    }
+
+
+}
